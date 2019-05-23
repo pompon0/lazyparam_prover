8cdb83513925a85415f83e1d1dbaca9036ea279b
Grzegorz Prusak <pompon.pompon@gmail.com>
2019-05-23 08:11:42 +0200

    implemented revertible stack

--- alloc.h
+++ alloc.h
@@ -2,12 +2,27 @@
 #define ALLOC_H_
 
 #include "types.h"
+#include "log.h"
+
+struct Snapshot {
+  size_t blocks_used;
+  u64 *begin,*end;
+};
+
+Snapshot stack{0,0,0};
 
 inline u64* alloc(size_t s) {
   enum { BLOCK = 1<<20 };
-  static u64 *begin = 0, *end = 0;
-  if(begin+s>end){ begin = new u64[BLOCK]; end = begin+BLOCK; }
-  auto ptr = begin; begin += s; return ptr;
+  DEBUG if(s>BLOCK) error("% = s > BLOCK = %",s,BLOCK);
+  static vec<u64*> blocks;
+  if(stack.begin+s>stack.end){
+    if(blocks.size()==stack.blocks_used) blocks.push_back(new u64[BLOCK]);
+    stack.begin = blocks[stack.blocks_used++];
+    stack.end = stack.begin+BLOCK;
+  }
+  auto ptr = stack.begin; stack.begin += s; return ptr;
 }
+
+
 
 #endif // ALLOC_H_
